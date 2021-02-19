70ff7b48f3a3afcc7afe6628b275d3afd1178c20
Marcos <marcos.sanchez@saei.mx>
2021-02-18 20:28:25 -0600

    imagen de fondo y cambio de pivot(test)

--- src/script.js
+++ src/script.js
@@ -3,6 +3,27 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+import { Vector3 } from 'three'
+
+//DOM
+const resistentesButton = document.querySelector('.button-0')
+const resistentesHide = document.querySelector('.hide-button-0')
+const otrosUsosButton = document.querySelector('.button-1')
+const promocionButton = document.querySelector('.button-2')
+const biodegradableButton = document.querySelector('.button-3')
+
+const resistentesVideo = document.querySelector('.video-0')
+
+resistentesButton.addEventListener('click', (e) => {
+    resistentesVideo.classList.toggle('hide')
+    resistentesHide.classList.toggle('hide')
+})
+
+resistentesHide.addEventListener('click', (e) => {
+    resistentesVideo.classList.toggle('hide')
+    resistentesHide.classList.toggle('hide')
+})
+
 
 /**
     TEXTURAS
@@ -14,11 +35,16 @@ const regioAmbientTexture = textureLoader.load('/textures/ao_map.png')
 const regioNormalTexture = textureLoader.load('/textures/normal.png')
 const regioRoughTexture = textureLoader.load('/textures/roughness.png')
 
+const screenShotPNG = textureLoader.load('/icons/camara.png')
+const screenRecordPNG = textureLoader.load('/icons/video.png')
+
 const bioPNG = textureLoader.load('/buttons/bio.png')
 const otrosPNG = textureLoader.load('/buttons/otros.png')
 const promoPNG = textureLoader.load('/buttons/promo.png')
 const regresarPNG = textureLoader.load('/buttons/regresar.png')
 const resistentesPNG = textureLoader.load('/buttons/resistentes.png')
+
+const bgTexture = textureLoader.load('/background/bg_threejs.png')
 
 // Debug
 //const gui = new dat.GUI()
@@ -28,10 +54,13 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = bgTexture
 
 //MODELS
 const objectLoader = new OBJLoader()
 
+
+const regioMeshGlobal = []
 objectLoader.load(
     '/models/regio/ToallasOBJ.obj',
     (gltf) => {
@@ -39,25 +68,28 @@ objectLoader.load(
         //console.log(gltf);
         const regioMesh = [...gltf.children]
         scene.add(regioMesh[0])
-
+        regioMeshGlobal.push(regioMesh[0])
         regioMesh[0].scale.set(0.5,0.5,0.5)
-        regioMesh[0].position.set(0,1.6,0)
+        regioMesh[0].position.set(-2,1.6,0)
         regioMesh[0].material.map = regioColorTexture
         regioMesh[0].material.transparent = true
         regioMesh[0].material.aoMap = regioAmbientTexture
         regioMesh[0].material.normalMap = regioNormalTexture
         regioMesh[0].material.roughnessMap = regioRoughTexture
-        
+        regioMesh[0].material.roughness = 1
+        regioMesh[0].material.metalness = 1
+        regioMesh[0].material.shininess = 50
     },
     () => {
         console.log('esta en progreso...');
     },
     () => {
-        console.log('hay un error :(');
+        console.log('hay un ERROR');
     }
 )
 
 //buttons
+/*
 const buttonsGroup = new THREE.Group()
 buttonsGroup.position.set(3,0,0)
 scene.add(buttonsGroup)
@@ -101,6 +133,7 @@ promoButton.position.set(2.5,1,0)
     })
 )
 regresarButton.position.set(6,3,0)*/
+/*
 
 const resistentesButton = new THREE.Mesh(
     new THREE.PlaneGeometry(2,1),
@@ -110,10 +143,34 @@ const resistentesButton = new THREE.Mesh(
         transparent: true
     })
 )
-resistentesButton.position.set(2.5,2.5,0)
+resistentesButton.position.set(2.5,2.5,0)*/
 
 
-buttonsGroup.add(bioButton,otrosButton,promoButton,resistentesButton)
+//buttonsGroup.add(bioButton,otrosButton,promoButton,resistentesButton)
+
+//SCREEN SHOT
+const screenShotButton = new THREE.Mesh(
+    new THREE.CircleGeometry(0.5,32),
+    new THREE.MeshStandardMaterial({
+        map:screenShotPNG,
+        side: THREE.DoubleSide,
+        transparent: true
+    })
+)
+screenShotButton.position.set(0-1,0.5,1.5)
+
+const screenRecordButton = new THREE.Mesh(
+    new THREE.CircleGeometry(0.5,32),
+    new THREE.MeshStandardMaterial({
+        map: screenRecordPNG,
+        side: THREE.DoubleSide,
+        transparent: true   
+    })
+)
+screenRecordButton.position.set(1,0.5,1.5)
+
+//scene.add(screenShotButton,screenRecordButton)
+
 /**
  * Floor
  */
@@ -149,13 +206,13 @@ directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
 const pointLight = new THREE.PointLight('white')
-pointLight.position.set(0,3,3)
+pointLight.position.set(0,3,-3)
 //scene.add(pointLight)
 
 //HELPERS
 
 const axisHelper = new THREE.AxisHelper(2)
-//scene.add(axisHelper)
+scene.add(axisHelper)
 
 const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
 //scene.add(pointLightHelper)
@@ -188,7 +245,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 4, 4)
+camera.position.set(0, 4, 5.5)
 scene.add(camera)
 
 // Controls
@@ -199,6 +256,7 @@ controls.minPolarAngle = 1.3; // radians
 controls.maxPolarAngle = 0.5; // radians
 controls.target.set(0, 0.75, 0)
 controls.enableDamping = true
+//controls.target.set()
 
 /**
  * Renderer
@@ -210,6 +268,9 @@ renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//raycast
+
 
 /**
  * Animate
